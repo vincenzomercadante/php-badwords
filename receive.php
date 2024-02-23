2a7b7357328e5ffb93aa6ef3bfb9ee657050d084
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-02-23 17:44:02 +0100

    feat: add comments

--- receive.php
+++ receive.php
@@ -1,9 +1,12 @@
 
 <?php 
 
-$paragraph = $_POST["paragraph"];
-$badword  = $_POST["badword"];
+// recover the paragraph
+$paragraph = trim($_POST["paragraph"]);
+// recover the bad
+$badword  = ($_POST["badword"]);
 
+// replacing the bad wordword
 $new_paragraph = str_replace($badword, ' ***', $paragraph);
 
 ?>
@@ -25,25 +28,38 @@ $new_paragraph = str_replace($badword, ' ***', $paragraph);
         crossorigin="anonymous"
         />
 
+        <!-- my style -->
         <link rel="stylesheet" href="./styles/receive.css" />
     </head>
     <body>
         <div class="container">
-            <h1 class="text-success mb-3">RESULT:</h1>
+            <h1 class="text-success mb-3 text-center">RESULT:</h1>
             <div class="row">
                 <div class="col-6 text-center">
-                    <h2 class="mb-3"> First Paragraph </h2>
-                    <p class="mb-3">
-                        <?= $paragraph ?>
-                    </p>
-                    <span><?php echo strlen($paragraph)?></span>
+                    <!-- card -->
+                    <div class="card">
+                        <!-- card-title -->
+                        <h2 class="mb-3"> First Paragraph </h2>
+                        <!-- card paragraph -->
+                        <p class="mb-3">
+                            <?= $paragraph ?>
+                        </p>
+                        <!-- paragraph length -->
+                        <span><?php echo strlen($paragraph)?></span>
+                    </div>
                 </div>
                 <div class="col-6 text-center">
-                    <h2 class="mb-3"> Second Paragraph </h2>
-                    <p class="mb-3">
-                        <?= $new_paragraph ?>
-                    </p>
-                    <span><?php echo strlen($new_paragraph)?></span>
+                    <!-- card -->
+                    <div class="card">
+                        <!-- card-title -->
+                        <h2 class="mb-3"> Second Paragraph </h2>
+                        <!-- card modified paragraph -->
+                        <p class="mb-3">
+                            <?= $new_paragraph ?>
+                        </p>
+                        <!-- modified paragraph length -->
+                        <span><?php echo strlen($new_paragraph)?></span>
+                    </div>
                 </div>
             </div>
         </div> 
